ae9a281c5d94a92699e5580cd6d7e273efd169e4
meuterer <sextus@gmail.com>
2014-03-26 21:26:53 -0400

    Initial implementation
    Implemented a sparse, console front end to our API to facilitate testing
    before moving on to a more robust implementation. To do: watchdog event
    logging

---
+++ oneDirConnection.py
@@ -0,0 +1,72 @@
+import requests
+import json
+from os.path import join,expanduser
+
+class OneDirConnection:
+    """Class to facilitate managing network communication between the oneDir client and server."""
+
+    def __init__(self, host):
+        """Constructor which setsup the host the server is at"""
+        self.host = host
+        self.cookie = None
+        self.user = None
+        home = expanduser("~")
+        self.onedirrectory = join(home, "oneDir")
+
+
+    def register(self, username ,password, email):
+        """Register the given username , password and email with the oneDir server"""
+        #build the request to send.
+        url = self.host + "register"
+        data = {'username': username, 'password': password, 'email': email}
+        headers = {'Content-Type': 'application/json'}
+        results = requests.post(url, data=json.dumps(data), headers=headers)
+
+        #If it returns username it worked otherwise we have an issue. Update with more pertinent data once server works for me
+        if results.json()['result'] == username:
+            return 1
+        else:
+            return -1
+
+    def login(self, username, password):
+        """Login to the oneDir server saving the cookie internally to preserve session"""
+        url = self.host + "session"
+        data = {'username': username, 'password': password}
+        headers = {'Content-Type': 'application/json'}
+        results = requests.post(url, data=json.dumps(data), headers=headers)
+        self.cookies = results.cookies
+        if results.json()['result'] == 1:
+            self.user = username
+            return 1
+        else:
+            return -1
+
+    def sendfile(self, path):
+        """Sends a file to the OneDir server using the internal cookie stored inside"""
+        url = self.host + 'file' + path
+        results = requests.post(url,  files={'file': open(join(self.onedirrectory + path), 'rb')}, cookies=self.cookies)
+        if results.json()['result'] == -1:
+            return -1
+        else:
+            return 1
+
+    def getfile(self, path):
+        """Gets a file from the OneDir server using the internal cookie stored inside"""
+        url = self.host + 'file' + path
+        results = requests.get(url, cookies=self.cookie)
+        if results.json()['result'] == -1:
+            return -1
+        else:
+            return 1
+
+    def logout(self):
+        """Logout from the oneDir api server"""
+        url = self.host + "session"
+        headers = {'Content-Type': 'application/json'}
+        result = requests.delete(url, cookies=self.cookies, headers=headers)
+        self.cookies = None
+        if result.json()['result'] == self.user:
+            return 1
+        else:
+            return -1
+
---
+++ onedirClient.py
@@ -0,0 +1,107 @@
+__author__ = 'team33'
+
+import requests
+import json
+import os
+import watchdog
+from os.path import expanduser
+from oneDirConnection import OneDirConnection
+
+
+def register(oneDir):
+        """Handles registration interactions with the user"""
+        print "You selected register. Please enter exit to quit or login to try to login:"
+        username = raw_input("Please enter your username")
+        if(str.lower(username) == 'exit'):
+            exit(1)
+        password = raw_input("Please enter your password")
+        email = raw_input("Please enter your email")
+        result = oneDir.register(username,password,email)
+        if result == 1:
+            print "Thank you for registering, " + username
+        else:
+            print "Please try again:"
+
+
+def login(oneDir):
+    """Handles individual login interactions"""
+    print "You selected login. Please enter exit to quit:"
+    username = raw_input("Please enter your username")
+    if(str.lower(username) == 'exit'):
+            exit(1)
+    password = raw_input("Please enter your password")
+    result = oneDir(username,password)
+    if result == 1:
+        print "Thank you for logging in!"
+        return True
+    else:
+        print "Issues logging in. Please try again, " + username
+        return False
+
+
+def prompt(oneDir):
+    """Prompt to handle initial login/registration"""
+    print "Welcome to the OneDir Client"
+    success = False
+    while True:
+        action = raw_input('login, register, or exit?')
+        if str.lower(action) == 'register':
+            register(oneDir)
+        if str.lower(action) == "login":
+            success = login(oneDir)
+        if str.lower(action) == "exit":
+            print "Thank you for trying OneDir"
+            exit(1)
+        #User is logged in
+        if success:
+            return
+
+
+def mainprompt(oneDir, pathtoonedir):
+        """Main prompt once user has logged in"""
+        userInput = raw_input("Please select an option, " + oneDir.user + " logout,exit, get or send")
+        if str.lower(userInput) == 'exit':
+            oneDir.logout()
+            exit(1)
+        if str.lower(userInput) == 'logout':
+            oneDir.logout()
+            prompt(oneDir)
+        if str.lower(userInput) == 'send':
+            filetosend = getfilename('send')
+            if oneDir.sendfile(filetosend) == 1:
+                print "File sent!"
+            else:
+                print "Issue sending file"
+        if str.lower(userInput) == 'get':
+            filetosend = getfilename('get')
+            if oneDir.getfile(filetosend) == 1:
+                print "File gotten!"
+            else:
+                print "Issue retrieving file"
+        else:
+            print "Please enter a valid option!"
+
+
+def getfilename(typeoffile):
+    """Handles promptin for file names"""
+    if typeoffile == 'get':
+        fileinfo = raw_input("Which file would you like to retrieve relative your oneDir directory:")
+    elif typeoffile == 'send':
+        fileinfo = raw_input("Which file would you like to send relative  to your oneDir directory:")
+    return fileinfo
+
+
+def main():
+    #host we're connecting to localhost for server on same machine
+    host = 'http://127.0.0.1:5000/'
+    client = OneDirConnection(host)
+    home = expanduser("~")
+    oneDir = os.path.join(home, "oneDir")
+    prompt(client)
+    #we are logged in now and the OneDirConnection has an internal cookie
+    while True:
+        mainprompt(client, oneDir)
+
+if __name__ == '__main__':
+    main()
+
