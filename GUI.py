605becb07c87de1718b132d454f5cac9651e7299
Will Emmanuel <will.emmanuel@gmail.com>
2014-04-21 14:35:05 -0400

    Initial login gui

--- GUI.py
+++ GUI.py
@@ -1,85 +1,43 @@
-from Tkinter import *
 
-class App:
-
-    def __init__(self, master):
-
-        frame = Frame(master)
-        frame.pack()
-
-        self.button = Button(
-            frame, text="Pull", fg="blue", command=self.pull
-            )
-        self.button.pack(side=LEFT)
-
-        self.textField = Entry(frame)
-        self.textField.pack(side=RIGHT)
-        self.v=StringVar()
-        self.w=Label(frame, textvariable=self.v)
-        self.w.pack()
-
-    def pull(self):
-        self.v.set("Searching for your file...")
-class PWChange:
-    def __init__(self, master):
-        frame=Frame(master)
-        frame.pack()
-        self.userLabel=Label(frame, text="new Username:")
-        self.userName=Entry(frame)
-        self.userLabel.pack()
-        self.userName.pack()
-        self.pwLabel=Label(frame, text="new Password:")
-        self.pwName=Entry(frame)
-        self.pwLabel.pack()
-        self.pwName.pack()
-        self.button=Button(frame, text="Create User", command=self.check)
-        self.button.pack()
-        self.varname=StringVar()
-        self.w=Label(frame, textvariable=self.varname)
-        self.w.pack()
-    def check(self):
-        f=open('users.txt', 'r')
-        item=[self.userName.get(), self.pwName.get()]
-        for line in f:
-            if line==str(item)+'\n':
-                self.varname.set('User Already Exists!')
-                f.close()
-                return
-        f.close()
-        with open("users.txt", "a") as f:
-            self.varname.set('User Created')
-            f.write(str(item)+'\n')
-root = Tk()
-w=Entry(root)
-wl=Label(root, text="username:")
-wl.pack()
-w.pack()
-vl=Label(root, text="password:")
-v=Entry(root)
-vl.pack()
-v.pack()
-varname=StringVar()
-warn=Label(root, textvariable=varname)
-warn.pack()
-def access():
-    users=[]
-    f=open('users.txt', 'r')
-    for line in f:
-        users.append(line)
-
-    item=[w.get(),v.get()]
-    if str(item)+'\n' in users:
-        top=Toplevel()
-        app = App(top)
+try:
+    import tkinter as tk
+except ImportError:
+    import Tkinter as tk
+# from http://effbot.org/tkinterbook/entry.htm
+passwords = [('DaniWeb', 'best1'), ('newbie', 'help!help!')]
+def make_entry(parent, caption, width=None, **options):
+    tk.Label(parent, text=caption).pack(side=tk.TOP)
+    entry = tk.Entry(parent, **options)
+    if width:
+        entry.config(width=width)
+    entry.pack(side=tk.TOP, padx=10, fill=tk.BOTH)
+    return entry
+def enter(event):
+    check_password()
+def check_password():
+    """ Collect 1's for every failure and quit program in case of failure_max failures """
+    print(user.get(), password.get())
+    if (user.get(), password.get()) in passwords:
+        root.destroy()
+        print('Logged in')
+        return
     else:
-        varname.set('Incorrect Username/Password')
+        error.pack(side=tk.TOP)
 
-def new():
-    top=Toplevel()
-    app=PWChange(top)
-b=Button(root, text="->", command= access)
-newUserButton=Button(root, text="Create New User", command=new)
-b.pack()
-newUserButton.pack()
-root.mainloop()
-root.destroy() # optional; see description below
+root = tk.Tk()
+root.geometry('200x250')
+root.title('OneDir')
+#frame for window margin
+parent = tk.Frame(root, padx=10, pady=10)
+parent.pack(fill=tk.BOTH, expand=True)
+#entrys with not shown text
+tk.Label(parent, text="Welcome to OneDir", height=2).pack(side=tk.TOP)
+user = make_entry(parent, "User name:", 16)
+password = make_entry(parent, "Password:", 16, show="*")
+error = tk.Label(parent, text="Incorrect combination", fg="red")
+#button to attempt to login
+b = tk.Button(parent, borderwidth=4, text="Login", width=10, pady=8, command=check_password)
+b.pack(side=tk.BOTTOM)
+password.bind('<Return>', enter)
+user.focus_set()
+parent.mainloop()
